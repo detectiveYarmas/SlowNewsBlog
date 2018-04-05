050a8ed22d21efea78e96fb159bad16598cea864
robert v sheran <robertsheran@icloud.com>
2018-04-05 15:11:48 -0500

    bobobobobobobob

--- SlowNewsBlog/SlowNewsBlog/Controllers/BloggerController.cs
+++ SlowNewsBlog/SlowNewsBlog/Controllers/BloggerController.cs
@@ -114,6 +114,7 @@ namespace SlowNewsBlog.Controllers
                 
                 blog.CatagoryId = model.Catagory.CatagoryId;
                 blog.BlogPostHashTags = model.BlogPost.BlogPostHashTags;
+                blog.Approved = false;
                 repo.UpdateBlogPost(blog);
                 return RedirectToAction("EditPost");
             }
